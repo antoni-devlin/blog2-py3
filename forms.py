7fefb5d0d317318c3f128fd6db673a64427bd93f
antoni-devlin <antoni.devlin@gmail.com>
2018-09-09 16:36:15 +0100

    Added comments and removed useless code

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 
 
-# Form for adding new posts
+# Add new post or edit exisiting post form
 class AddEditPost(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     category = SelectField(u'Category', choices=[('video', 'Video'), ('tech', 'Tech'), ('food', 'Food'), ('writing', 'Writing')])
@@ -11,12 +11,14 @@ class AddEditPost(FlaskForm):
     body = TextAreaField('Content Area')
     submit = SubmitField('Save Post')
 
+# Login Form
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+#Register Form
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
